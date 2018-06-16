e9d48fd2a47c41daaf87c06513387c15be8d20c8
nicbh <642374509@qq.com>
2018-06-16 20:17:35 +0800

    reload_utf-8

--- bt.py
+++ bt.py
@@ -9,13 +9,21 @@ from bs4 import BeautifulSoup
 from urllib.parse import unquote, quote
 from flask import Flask, request
 from selenium.common.exceptions import TimeoutException
-import requests, json, time, random, sys, os, time
+import requests
+import json
+import time
+import random
+import sys
+import os
+import time
 
 timeout_start = 20
 timeout_max = 60
 
+
 def sleep(down, up):
     time.sleep(random.random() * (up - down) + up)
+
 
 def open_site(browser, func):
     url = ''
@@ -26,7 +34,8 @@ def open_site(browser, func):
     timeout = timeout_start
     while success is False and timeout <= timeout_max:
         try:
-            print('[INFO {}] start to loading {} in {}s'.format(time.ctime(), url, timeout))
+            print('[INFO {}] start to loading {} in {}s'.format(
+                time.ctime(), url, timeout))
             sys.stdout.flush()
             if timeout == timeout_start:
                 func(browser)
@@ -34,7 +43,8 @@ def open_site(browser, func):
                 browser.refresh()
             success = True
         except TimeoutException:
-            print('[INFO {}] time out after {} seconds when loading'.format(time.ctime(), timeout))
+            print('[INFO {}] time out after {} seconds when loading'.format(
+                time.ctime(), timeout))
             sys.stdout.flush()
             try:
                 browser.execute_script('window.stop()')
@@ -47,48 +57,6 @@ def open_site(browser, func):
                 browser.set_script_timeout(timeout)
     browser.set_page_load_timeout(timeout_start)
     browser.set_script_timeout(timeout_start)
-
-
-def getMagnet(contentUrl, browser):
-    sleep(0.3, 0.7)
-    open_site(browser, contentUrl)
-    soup = BeautifulSoup(browser.page_source, 'html.parser')
-    magnet = soup.find(class_='magnet')
-    return magnet.a.attrs['href']
-
-def getResouce(html, browser):
-    resource = []
-    soup = BeautifulSoup(html, 'html.parser')
-    list = soup.find_all('dl')
-    num = 3
-    index = 0
-    if len(list) < num:
-        num = len(list)
-    for item in list[0:num]:
-        href = item.dt.a
-        if href is None:
-            return None
-        href = item.dt.a.attrs['href']
-        spans = item.find(class_='option').find_all('span')
-        magnet = getMagnet(spans[1].a.attrs['href'], browser)
-        name = unquote(magnet[magnet.rfind('=') + 1:])
-        magnet = magnet[0:magnet.rfind('&dn=')]
-        time_ = spans[2].b.string
-        volume = spans[3].b.string
-        files = spans[4].b.string
-        hots = spans[6].b.string
-        index += 1
-        resource.append({
-            'name': name,
-            'num': index,
-            'href': href,
-            'magnet': magnet,
-            'time': time_,
-            'volume': volume,
-            # 'file': files,
-            'hot': hots
-        })
-    return resource
 
 
 from selenium import webdriver
@@ -105,15 +73,57 @@ chrome_options.binary_location = '/opt/google/chrome/chrome'
 prefs = {
     'profile.default_content_setting_values': {
         'images': 2,
-        'javascript':2
+        'javascript': 2
     }
 }
-chrome_options.add_experimental_option('prefs',prefs)  
+chrome_options.add_experimental_option('prefs', prefs)
 
 app = Flask(__name__)
 
+
 @app.route('/search2', methods=['POST'])
 def search_v2(keyword=None):
+    def getMagnet(contentUrl, browser):
+        sleep(0.3, 0.7)
+        open_site(browser, contentUrl)
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        magnet = soup.find(class_='magnet')
+        return magnet.a.attrs['href']
+
+    def getResouce(html, browser):
+        resource = []
+        soup = BeautifulSoup(html, 'html.parser')
+        list = soup.find_all('dl')
+        num = 3
+        index = 0
+        if len(list) < num:
+            num = len(list)
+        for item in list[0:num]:
+            href = item.dt.a
+            if href is None:
+                return None
+            href = item.dt.a.attrs['href']
+            spans = item.find(class_='option').find_all('span')
+            magnet = getMagnet(spans[1].a.attrs['href'], browser)
+            name = unquote(magnet[magnet.rfind('=') + 1:])
+            magnet = magnet[0:magnet.rfind('&dn=')]
+            time_ = spans[2].b.string
+            volume = spans[3].b.string
+            files = spans[4].b.string
+            hots = spans[6].b.string
+            index += 1
+            resource.append({
+                'name': name,
+                'num': index,
+                'href': href,
+                'magnet': magnet,
+                'time': time_,
+                'volume': volume,
+                # 'file': files,
+                'hot': hots
+            })
+        return resource
+
     response = None
     browser = webdriver.Chrome(chrome_options=chrome_options)
     try:
@@ -127,7 +137,8 @@ def search_v2(keyword=None):
         # sleep(0.5, 1.5)
         browser.find_element_by_id('kwd').send_keys(keyword)
         sleep(0.5, 1)
-        open_site(browser, lambda b: b.find_element_by_id('kwd').send_keys(Keys.ENTER))
+        open_site(browser, lambda b: b.find_element_by_id(
+            'kwd').send_keys(Keys.ENTER))
         url = browser.current_url
         print(url)
         subclass = ['4']
@@ -195,9 +206,12 @@ if __name__ == '__main__':
         argv.pop(0)
     if len(argv) > 0:
         if argv[0].lower() == 'stop':
-            os.system("kill $(ps aux | grep 'chromedrive[r]' | awk '{print $2}')")
+            os.system(
+                "kill $(ps aux | grep 'chromedrive[r]' | awk '{print $2}')")
             os.system("kill $(ps aux | grep 'bt.p[y]' | awk '{print $2}')")
         elif argv[0].lower() == 'kw':
+            reload(sys)
+            sys.setdefaultencoding("utf-8")
             print('start to search keyword "{}"...'.format(argv[1]))
             print(search_v2(argv[1]))
     else:
